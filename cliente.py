48b8b381289dce7685f6f46c135978ca1f50e099
Livany <40469622+livanynunes@users.noreply.github.com>
2019-09-24 16:25:33 -0300

    Finalização!

--- cliente.py
+++ cliente.py
@@ -4,7 +4,15 @@ import socket
 from calendar import*;
 from time import*;
 
+#função socket, da classe socket:
+#Os parâmetro são familia A_INET, que significa IPV4
+##E o tipo. que pode ser:
+#O SOCK_STREAM = tcp
+#O SOCK_DGRAM = UDP
 cliente = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+#Retorna string com data e horas atual, no formato
+#p.e. Sun Apr 23 08:45:32 2019 - Dia da semana, mês, Dia do mês, hora, minutos, segundos e ano
 
 mensagem_envio = asctime(localtime(time()))
 
--- servidor.py
+++ servidor.py
@@ -1,12 +1,31 @@
 import socket
 
+#função socket, da classe socket:
+#Os parâmetro são familia A_INET, que significa IPV4
+##E o tipo. que pode ser:
+#O SOCK_STREAM = tcp
+#O SOCK_DGRAM = UDP
 servidor = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-servidor.bind(('',12000))
 
+#Bind é o edereço, se a familia or do tipo AF_INET os paramêtros precisam
+# ser o (host,porta)
+
+servidor.bind(('',12000)) #host = própria máquina
+
+# loop para fficar sempre à escuta
 while True:
+	#recvfrom parametro é o tamanho do buffer de recebimento, aconselhado 2048
+	#recebe mensagem em bytes e endereço
 	mensagem_bytes, endereco_ip_client = servidor.recvfrom(2048)
-	mensagem_resposta = mensagem_bytes.decode().upper()
-	servidor.sendto(mensagem_resposta.encode(),endereco_ip_client)
+
+	#converte a mensagem em bytes e conevrte para string
+	mensagem_resposta = mensagem_bytes.decode()
+
+	mensagem_confirma = "Imprimi a hora"
+	#envia mensagem de confirmação em bytes para o endereço do cliente
+	servidor.sendto(mensagem_confirma.encode(),endereco_ip_client)
+
+	#imprime a hora
 	print(mensagem_resposta)
 #-----------------------------------------------------------------
 #
